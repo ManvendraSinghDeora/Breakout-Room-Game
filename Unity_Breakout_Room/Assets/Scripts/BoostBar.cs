6de38913cb2b7f22dc2ad8e34c6c352ccb70a8aa
Grimoc <47793416+Grimoc@users.noreply.github.com>
2020-11-12 02:31:51 +0530

    Update
    Added Car
    Camera Movements
    Boost Bar

---
+++ Unity_Breakout_Room/Assets/Scripts/BoostBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+using VehicleBehaviour;
+
+public class BoostBar : MonoBehaviour
+{
+    Image _boostBar;
+    public WheelVehicle _PlayerRef; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _boostBar = GetComponent<Image>();
+        _boostBar.fillAmount = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _boostBar.fillAmount = _PlayerRef.Boost/_PlayerRef.MaxBoost;
+    }
+}
---
+++ Unity_Breakout_Room/Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 Offset;
+    public float CameraMoveSpeed;
+    void Start()
+    {
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, Target.position + Offset, CameraMoveSpeed*Time.deltaTime);
+    }
+}
