cc218d7cf262c25314c13b808da8df93dba40d47
Grimoc <47793416+Grimoc@users.noreply.github.com>
2020-11-24 13:09:54 +0530

    materialupdate

---
+++ Unity_Breakout_Room/Assets/Hallow/Script/BulletScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    #region Public Declarations
+
+    public int Damage;
+    public float Speed;
+    private Vector3 Direction;
+    private Rigidbody rb;
+    #endregion
+
+
+    public void Setup(Vector3 dir)
+    {
+        Direction = dir;
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rb.velocity = Direction * Speed * Time.deltaTime;
+    }
+
+
+    private void OnCollisionEnter(Collision _collision)
+    {
+        PlayerStats.Instance.Health -= Damage;
+        Destroy(this.gameObject);
+    }
+    private void OnTriggerEnter(Collider _collider)
+    {
+
+    }
+}
---
+++ Unity_Breakout_Room/Assets/Hallow/Script/Gun.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject GunPointer;
+    public GameObject GunBackside;
+    public GameObject BulletPrefab;
+
+
+    private void Start()
+    {
+        if (GunBackside == null || GunPointer == null)
+        {
+            Debug.LogError("Define Gun Pointer and GunBackside");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            var Bul = Instantiate(BulletPrefab, GunPointer.transform.position, Quaternion.identity);
+            Bul.GetComponent<BulletScript>().Setup(directioncal());
+
+        }
+    }
+
+    Vector3 directioncal()
+    {
+        return (GunPointer.transform.position - GunBackside.transform.position).normalized;
+    }
+   
+}
